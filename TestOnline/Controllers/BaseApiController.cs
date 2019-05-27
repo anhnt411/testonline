7811b7ff95174590fe0b305f38934cedab0024d6
TuanAnh <anh.nt.1196@gmail.com>
2019-05-28 00:58:32 +0700

    okok

--- TestOnline/Controllers/BaseApiController.cs
+++ TestOnline/Controllers/BaseApiController.cs
@@ -4,12 +4,74 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.WebSockets.Internal;
+using TestOnline.Object;
+using TestOnlineBase.Constant;
+using TestOnlineBase.Enum;
 
 namespace TestOnline.Controllers
 {
    
     public class BaseApiController : ControllerBase
     {
+        [ApiExplorerSettings(IgnoreApi = true)]
+        public IActionResult InternalServerErrorResult(object className, string methodName, string info = null, string errorMessage = null)
+        {
+            var resultObject = new ResultObject()
+            {
+                Message = Constant.Message.SERVER_ERROR,
+                Result = null,
+                StatusCode = Enums.StatusCode.Error
+            };
+            return Ok(resultObject);
+        }
 
+        [ApiExplorerSettings(IgnoreApi = true)]
+        public IActionResult DataValidationError(string message)
+        {
+            var resultObject = new ResultObject()
+            {
+                Message = message,
+                Result = null,
+                StatusCode = Enums.StatusCode.Error
+            };
+            return Ok(resultObject);
+        }
+
+        [ApiExplorerSettings(IgnoreApi = true)]
+        public IActionResult NotFoundErrorResult()
+        {
+            var resultObject = new ResultObject()
+            {
+                Message = Constant.Message.NOTFOUND_RESULT,
+                Result = null,
+                StatusCode = Enums.StatusCode.Error
+            };
+            return Ok(resultObject);
+        }
+
+        [ApiExplorerSettings(IgnoreApi = true)]
+        public IActionResult ForbiddenErrorResult()
+        {
+            var resultObject = new ResultObject()
+            {
+                Message = Constant.Message.FORBIDDENRESULT,
+                Result = null,
+                StatusCode = Enums.StatusCode.Forbidden
+            };
+            return Ok(resultObject);
+        }
+
+        [ApiExplorerSettings(IgnoreApi = true)]
+        public IActionResult FailedProcessingErorrResult()
+        {
+            var resultObject = new ResultObject()
+            {
+                Message = Constant.Message.FAILDPROCESSINGRESULT,
+                Result = null,
+                StatusCode = Enums.StatusCode.Error
+            };
+            return Ok(resultObject);
+        }
     }
 }
--- TestOnlineBase/Constant/Constant.cs
+++ TestOnlineBase/Constant/Constant.cs
@@ -19,6 +19,11 @@ namespace TestOnlineBase.Constant
             public const string PARAMETER_IS_NOT_VALID = "Parameter is not valid";
             public const string GET_DATA_SUCCESSFULLY = "Get data successfully";
             public const string SAVE_DATA_SUCCESSFULLY = "Save data successfully";
+            public const string SERVER_ERROR = "InternalServerError";
+            public const string NOTFOUND_RESULT = "NotFound";
+            public const string FORBIDDENRESULT = "ForbiddenError";
+            public const string FAILDPROCESSINGRESULT = "InternalServerError";
+
 
         }
         #endregion
