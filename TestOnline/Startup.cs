71451c15f35346599152b684ccdbd3f2f6dafca0
TuanAnh <anh.nt.1196@gmail.com>
2019-05-28 23:55:37 +0700

    ok

--- TestOnline/Startup.cs
+++ TestOnline/Startup.cs
@@ -6,10 +6,12 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using TestOnlineEntity.Model.Context;
 
 namespace TestOnline
 {
@@ -43,6 +45,11 @@ namespace TestOnline
 
             app.UseHttpsRedirection();
             app.UseMvc();
+        }
+
+        private void AddService(IServiceCollection services)
+        {
+            services.AddDbContext<TestOnlineDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("sqlServerConnectionString")));
         }
     }
 }
