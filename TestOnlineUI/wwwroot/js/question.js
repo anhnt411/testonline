f7c0241ed276ff6fe0785802defaf69ff9c17d58
nguyen anh <anh.nt.1196@gmail.com>
2019-07-23 00:31:29 +0700

    ok

--- TestOnlineUI/wwwroot/js/question.js
+++ TestOnlineUI/wwwroot/js/question.js
@@ -6,11 +6,25 @@
         registerEvent: function () {
           
             CKEDITOR.replace('questionContent', {
-                customConfig: '/js/CustomConfig.js',
-
-            
-                //filebrowserUploadUrl: '/Home/UploadCKEditor'
+                customConfig: '/js/CustomConfig.js',                     
             });
+
+                                
+            var templateHtml = `<div class="editor">
+                                    <div class="editor_content">
+                                        <span> Đáp án A </span> <span style="color:red;">*</span>
+                                    </div>
+                                    <textarea id="answerContent1" name="Content" class="ckeditor"></textarea>
+                                    <select name="IsCorrect" class="isCorrect">
+                                        <option value="false" selected>Sai</option>
+                                        <option value="true">Đúng</option>
+                                    </select>
+                                    <br />
+                                    <button class="addanswer">+</button><button class="removeanswer">-</button>
+                                    <hr />
+                                </div>`;
+                               
+
             
         }
     };
